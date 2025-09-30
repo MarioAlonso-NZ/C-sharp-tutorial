236d93cd4c2b3b1cde4e727401903bd6151aabef
MarioAlonso-NZ <mario.nezzvla@gmail.com>
2025-09-30 13:48:23 -0600

    feat: Creation of a namespace to organize better our Company code starting with Employee class. I call this namespace later on OOP-UsingCompanyNamespace.cs

---
+++ Code/OOP-UsingCompanyNamespace.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Author: Mario N.Zavala
+ * Date: September 2025
+ * Description: Call the Employee class from the Company namespace and display employee information.
+ * This demostrastes the use of namespaces to organize code.
+ */
+using Oriented_Object_Programming.Company;
+
+Employee employee1 = new Employee()
+{
+    Id = -1,
+    Name = "John Doe",
+    Salary = 20000.00m
+};
+Employee employee2 = new Employee(209, "Jane Smith", 25000.00m);
+
+Console.WriteLine(employee1.GetInfo());
+Console.WriteLine(employee2.GetInfo());
